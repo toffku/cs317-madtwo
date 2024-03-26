fac2f911f66cbf45835dd9e89b7ea29b942f7672
Kristoff Urcia SE2021 <kristoff.urcia.2021@uni.strath.ac.uk>
2024-03-26 12:40:42 +0000

    finalising commits

--- components/HomeContent.js
+++ components/HomeContent.js
@@ -16,42 +16,36 @@ import FontComponent from "./FontComponent";
 
 const HomeContent = () => {
   const navigation = useNavigation();
-
   const handleTools = () => {
     navigation.navigate("Tools");
   };
-
   const handleMap = () => {
     navigation.navigate("Map");
   };
-
   return (
-    <View className=" px-5 justify-end">
+    <View className="flex-1 px-5 justify-end">
+      {/* Sections */}
       <TouchableOpacity
-        className="w-full h-24 rounded-lg mb-4 p-3 justify-around items-center"
+        className="w-full h-24 rounded-lg items-center justify-center mb-5 p-5"
         style={GlobalStyles.bgColor}
         onPress={handleMap}
       >
         <MaterialCommunityIcons
           name="map-marker-radius"
           color={themeColor}
-          size={40}
+          size={30}
         />
-        <FontComponent className=" text-[12px]" style={GlobalStyles.themeText}>
+        <FontComponent className="mt-1" style={GlobalStyles.themeText}>
           Find a gym near me
         </FontComponent>
       </TouchableOpacity>
       <TouchableOpacity
-        className="w-full h-24 rounded-lg mb-4 p-3 overflow-hidden justify-around items-center"
+        className="w-full h-24 rounded-lg items-center justify-center mb-5"
         style={GlobalStyles.bgColor}
         onPress={handleTools}
       >
-        <MaterialCommunityIcons
-          name="hammer-wrench"
-          color={themeColor}
-          size={40}
-        />
-        <FontComponent className="text-[12px]" style={GlobalStyles.themeText}>
+        <MaterialCommunityIcons name="tools" color={themeColor} size={30} />
+        <FontComponent className="mt-1" style={GlobalStyles.themeText}>
           Tools
         </FontComponent>
       </TouchableOpacity>
